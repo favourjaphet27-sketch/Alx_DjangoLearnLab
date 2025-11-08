6706ba94a6e3923d2ce6dafb8470636a9528b218
Favour Japheth <favourjaphet27@gmail.com>
2025-11-08 14:32:40 -0800

    Fix: added correct import and url patterns for list_books, add_book and edit_book

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from .views import list_books
 from .views import (
-    list_books,
     LibraryDetailView,
     add_book,
     edit_book,
@@ -18,9 +18,9 @@ urlpatterns = [
     path(
         "library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"
     ),  # class-based view
-    path("books/add/", add_book, name="add_book"),
-    path("books/edit/<int:pk>/", edit_book, name="edit_book"),
-    path("books/delete/<int:pk>/", delete_book, name="delete_book"),
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/<int:pk>/", edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", delete_book, name="delete_book"),
     # Authentication views
     path("register/", register, name="register"),
     path(
