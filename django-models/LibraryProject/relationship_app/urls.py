5c953723ff76d15f638c1294c8b60d94205f1d37
Favour Japheth <favourjaphet27@gmail.com>
2025-11-08 08:48:55 -0800

    Made login and logout views classes instead of functions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,14 @@ from . import views
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),  # function based view
     path("register/", views.register, name="register"),
-    path("login/", views.user_login, name="login"),
-    path("logout/", views.user_logout, name="logout"),
+    path(
+        "login/",
+        views.UserLoginView.as_view(template_name="relationship_app/login.html"),
+    ),
+    path(
+        "logout/",
+        views.UserLogoutView.as_view(template_name="relationship_app/logout.html"),
+    ),
     path(
         "library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"
     ),  # class-based view
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.views.generic import DetailView
 from .models import Library, Book
@@ -40,22 +41,9 @@ def register(request):
 
 
 # Login view
-def user_login(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Login successful!")
-            return redirect("list_books")
-        else:
-            messages.error(request, "Invalid username or password.")
-    return render(request, "relationship_app/login.html")
+class UserLoginView(LoginView):
+    template_name = "relationship_app/login.html"
 
 
-# Logout view
-def user_logout(request):
-    logout(request)
-    messages.info(request, "You have been logged out.")
-    return redirect("login")
+class UserLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
