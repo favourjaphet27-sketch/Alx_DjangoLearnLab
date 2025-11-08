063e7cb33b501a3c4b5d46a17500e530d597a2ba
Favour Japheth <favourjaphet27@gmail.com>
2025-11-08 03:22:42 -0800

    trying to agree with alx checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # Must explicitly include Library
 
-# Create your views here.
-#Function based view: list all books
+
 def list_books(request):
+    """Function-based view that lists all books."""
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 class LibraryDetailView(DetailView):
+    """Class-based view for displaying a specific library and its books."""
+
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-    
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
     def get_context_data(self, **kwargs):
-        #inject additional context data for the library
         context = super().get_context_data(**kwargs)
         library = self.get_object()
-        context['books'] = library.books.all()
+        context["books"] = library.books.all()
         return context
-
