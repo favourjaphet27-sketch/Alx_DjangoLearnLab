25e045c668eb24589b4b6fa897fa80dee61792b6
Favour Japheth <favourjaphet27@gmail.com>
2025-11-08 09:53:46 -0800

    Added UserProfile model

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import UserProfile
 from django.views.generic import DetailView
 from .models import Library, Book
@@ -59,15 +59,27 @@ from .models import UserProfile
 
 # Function to check roles
 def is_admin(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+    return (
+        user.is_authenticated
+        and hasattr(user, "userprofile")
+        and user.userprofile.role == "Admin"
+    )
 
 
 def is_librarian(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+    return (
+        user.is_authenticated
+        and hasattr(user, "userprofile")
+        and user.userprofile.role == "Librarian"
+    )
 
 
 def is_member(user):
-    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+    return (
+        user.is_authenticated
+        and hasattr(user, "userprofile")
+        and user.userprofile.role == "Member"
+    )
 
 
 @user_passes_test(is_admin)
