c2655f9956b0b7c477e6713270227b6c11941475
Favour Japheth <favourjaphet27@gmail.com>
2025-11-29 12:07:26 -0800

    Worked on Book CRUD urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     path("books/", BookListView.as_view(), name="book-list"),
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
-    path("book/create/", BookCreateView.as_view(), name="book-create"),
-    path("book/update/<int:pk>/", BookUpdateView.as_view(), name="book-update"),
-    path("book/delete/<int:pk>/", BookDeleteView.as_view(), name="book-delete"),
+    path("books/create/", BookCreateView.as_view(), name="book-create"),
+    path("books/update/<int:pk>/", BookUpdateView.as_view(), name="book-update"),
+    path("books/delete/<int:pk>/", BookDeleteView.as_view(), name="book-delete"),
 ]
