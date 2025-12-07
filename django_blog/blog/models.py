2630bab2213d0141a5d327128f8b717b89f5dd2c
Favour Japheth <favourjaphet27@gmail.com>
2025-12-07 02:04:44 -0800

    Adjusted post model to import user from'django.contrib.auth.model'

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,4 +7,4 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     contents = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
